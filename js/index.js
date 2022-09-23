c6b9bd8b31b950a41db6e759907c504a9445443f
JuanMartinMN <juanmartinmn2001@gmail.com>
2022-09-23 15:30:18 -0300

    formulario de reservas completado

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
-document.querySelector('#submit').addEventListener('click',function(){
+document.querySelector('#reserva').addEventListener('click',function(){
     let nombre = document.querySelector('#name').value;
     let cantidad = document.querySelector('#mesa').value
     let fecha = document.querySelector('#date').value;
     let hora = document.querySelector('#hora').value;
 
+    // Date Format
+    let array = fecha.split('-');
+    month = array[1];
+    day = array[2];
+    let date = `${day}/${month}`;
 
-    const url = `https://api.whatsapp.com/send?phone=543815671888&text=Hola!, me gustaria hacer una reserva en Casa Croix para ${cantidad} personas el dia ${fecha} para ${hora} a nombre de ${nombre}. Muchas gracias!`;
+    const url = `https://api.whatsapp.com/send?phone=543816203040&text=Hola!, me gustaria hacer una reserva en Casa Croix para ${cantidad} persona/s el dia ${date} para ${hora} a nombre de ${nombre}. Muchas gracias!`;
 
     window.open(url)
 })
